c6c05532fbf7b186e4910ab0adae761bf27cb7ea
khang <chaoskid1415@gmail.com>
2025-07-30 11:13:47 +0700

    Khởi tạo 1 số module và kết nôi database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,40 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BookingModule } from './booking/booking.module';
+import { ReviewModule } from './reviews/reviews.module';
+import { RoomModule } from './room/room.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        //entities: [User],
+        synchronize: configService.get('database.synchronize'),
+        port: configService.get('database.port'),
+        username: configService.get('database.user'),
+        password: configService.get('database.password'),
+        host: configService.get('database.host'),
+        autoLoadEntities: configService.get('database.autoLoadEntities'),
+        database: configService.get('database.name'),
+      }),
+    }),
+    BookingModule,
+    ReviewModule,
+    RoomModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
